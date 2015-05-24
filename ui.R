801d1749bb5d19159bb8b8677df71cdbb36cade9
hpariyaram <hpariyaram@gmail.com>
2015-05-24 13:13:05 -0700

    final version

--- ui.R
+++ ui.R
@@ -6,6 +6,12 @@ library(shiny)
 
 shinyUI(fluidPage(
   titlePanel("Stock Analysis App"),
+  helpText("The StockAnalysisApp is a Shiny Application that accepts a stock ticker symbol
+and a date range from the user.  Then it pulls out the details of the stock
+from Google Finance Source and provide two stock analysis charts for that
+           particular ticker symbol.
+           More Information can be found at: "),
+  h5("Stock Analysis App Presentation", a("http://rpubs.com/hpariyaram/StockAnalysisApp", href="http://rpubs.com/hpariyaram/StockAnalysisApp")),
   
   fluidRow(
     column(6, 
